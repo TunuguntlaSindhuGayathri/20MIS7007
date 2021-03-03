f46935ac3de3dfa6756ed5d1d9fdd9d9cca14bde
Tunuguntla sindhu gayathri <79630868+TunuguntlaSindhuGayathri@users.noreply.github.com>
2021-03-03 16:09:22 +0530

    Update simple.java

--- simple.java
+++ simple.java
@@ -4,3 +4,14 @@ class Simple{
      System.out.println("first.java");   
     }  
 }  
+//Java Program to demonstate the use of if statement.  
+public class IfExample {  
+public static void main(String[] args) {  
+    //defining an 'age' variable  
+    int age=20;  
+    //checking the age  
+    if(age>18){  
+        System.out.print("Age is greater than 18");  
+    }  
+}  
+}  
