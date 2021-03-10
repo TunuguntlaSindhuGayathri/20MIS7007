57708a73d41b73ce89c8a63fa6bde3eb0b431bd8
sindhu gayathri <gayathri.20mis7007@vitap.ac.in>
2021-03-10 17:17:05 +0530

    made changes to first.java program

--- first.java
+++ first.java
@@ -1,11 +1,6 @@
-//This is empty file//Java Program to demonstate the use of if statement.  
-public class IfExample {  
+//This is an empty file  
+class first {  
 public static void main(String[] args) {  
-    //defining an 'age' variable  
-    int age=20;  
-    //checking the age  
-    if(age>18){  
-        System.out.print("Age is greater than 18");  
-    }  
-}  
+    System.out.println("Hello java");
+     }  
 } 
